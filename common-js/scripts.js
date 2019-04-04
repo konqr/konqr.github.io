3acc644d6bfaca7e6d50843244ba45b162e4abc9
Konark Jain <konark.145@iitg.ernet.in>
2019-04-04 18:55:46 +0530

    updated

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -191,6 +191,30 @@ function enableCounterUp(a){
 	return a;
 }
 
+document.getElementsByClassName('tablinks')[0].click()
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
 function isExists(elem){
 	if ($(elem).length > 0) { 
 		return true;
